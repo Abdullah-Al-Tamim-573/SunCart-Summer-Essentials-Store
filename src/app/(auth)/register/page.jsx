a39fdb7f20412cc8ba3df6b5cdd7cb9c6197a94f
Abdullah-Al-Tamim-573 <abdullahaltamim573@gmail.com>
2026-05-14 08:02:33 +0600

    add google sign un in log in and register page

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -17,11 +17,8 @@ import Link from "next/link";
 import { authClient } from "@/lib/auth-client";
 import { toast } from "react-toastify";
 
-
 export default function RegisterPage() {
-
-    let router = useRouter();
-
+  let router = useRouter();
 
   const {
     register,
@@ -33,23 +30,23 @@ export default function RegisterPage() {
   const onSubmit = async (registerData) => {
     let { email, name, password, photoUrl } = registerData;
 
-    const { data, error } = await authClient.signUp.email({
-      email, 
-      password, 
-      name, 
-      image: photoUrl,
-    },  { onSuccess: (ctx) => {
-            router.push('/logIn')
-            toast.success( 'You SuccessFully SignUp' );
-            
+    const { data, error } = await authClient.signUp.email(
+      {
+        email,
+        password,
+        name,
+        image: photoUrl,
+      },
+      {
+        onSuccess: (ctx) => {
+          router.push("/logIn");
+          toast.success("You SuccessFully SignUp");
         },
         onError: (ctx) => {
-            
-            
-            toast.error(ctx.error.message)
-        }
-  })
-   
+          toast.error(ctx.error.message);
+        },
+      },
+    );
   };
 
   return (
@@ -58,6 +55,26 @@ export default function RegisterPage() {
       className="flex w-[90%] md:w-150 flex-col gap-4 space-y-5 my-10 border p-10 rounded-xl mx-auto"
     >
       <h2 className="text-3xl font-bold text-center">Create a new Account</h2>
+      <button
+        aria-label="Login with Google"
+        type="button"
+        className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-600 focus:dark:ring-violet-600"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 32 32"
+          className="w-5 h-5 fill-current"
+        >
+          <path d="M16.318 13.714v5.484h9.078c-0.37 2.354-2.745 6.901-9.078 6.901-5.458 0-9.917-4.521-9.917-10.099s4.458-10.099 9.917-10.099c3.109 0 5.193 1.318 6.38 2.464l4.339-4.182c-2.786-2.599-6.396-4.182-10.719-4.182-8.844 0-16 7.151-16 16s7.156 16 16 16c9.234 0 15.365-6.49 15.365-15.635 0-1.052-0.115-1.854-0.255-2.651z"></path>
+        </svg>
+        <p>Login with Google</p>
+      </button>
+      <div className="flex items-center w-full my-4">
+        <hr className="w-full dark:text-gray-600" />
+        <p className="px-3 dark:text-gray-600">OR</p>
+        <hr className="w-full dark:text-gray-600" />
+      </div>
+
       {/* name */}
       <TextField isRequired name="name" type="text">
         <Label>Name</Label>
@@ -125,8 +142,11 @@ export default function RegisterPage() {
           Reset
         </Button>
       </div>
-      <div >
-           Already have an account? <Link className="text-[blue]" href={'logIn'}>Login</Link>
+      <div>
+        Already have an account?{" "}
+        <Link className="text-[blue]" href={"logIn"}>
+          Login
+        </Link>
       </div>
     </Form>
   );
